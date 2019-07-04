7a9173fea4af8952573f022c6c94ccaefcdb8675
Gustavo Lira <gustavo@grlira.com>
2019-07-04 21:23:10 +0200

    Add storybook

---
+++ src/stories/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Record from '../components/record';
+
+storiesOf('Record', module).add('default', () => <Record />);
